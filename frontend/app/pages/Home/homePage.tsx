3735709c2947fb35e062a24804ea9e5bca0178e7
Mfrozzz <marcosvinibva@gmail.com>
2025-05-14 14:51:50 -0300

    add logo to HomePage and update styling

--- frontend/app/pages/Home/homePage.tsx
+++ frontend/app/pages/Home/homePage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router'
+import logo from "../../assets/linkLogo.png";
 
 const navigation = [
 	{ name: 'New URL', href: '/' },
@@ -10,7 +11,7 @@ export default function HomePage() {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
 	return (
-		<div className="bg-white">
+		<div className="bg-blue-200">
 			<header className="absolute inset-x-0 top-0 z-50">
 				<nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
 					<div className="flex lg:flex-1">
@@ -18,10 +19,13 @@ export default function HomePage() {
 							<span className="sr-only">Your Company</span>
 							<img
 								alt=""
-								src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+								src={logo}
 								className="h-8 w-auto"
 							/>
 						</Link>
+						<p className='text-2xl pl-2 font-bold text-blue-900'>
+							Url Shortener
+						</p>
 					</div>
 					<div className="flex lg:hidden">
 						<button
@@ -56,32 +60,25 @@ export default function HomePage() {
 						className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
 					/>
 				</div>
-				<div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-					<div className="hidden sm:mb-8 sm:flex sm:justify-center">
-						<div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-							Announcing our next round of funding.{' '}
-							<a href="#" className="font-semibold text-indigo-600">
-								<span aria-hidden="true" className="absolute inset-0" />
-								Read more <span aria-hidden="true">&rarr;</span>
-							</a>
-						</div>
-					</div>
+				<div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56" style={{ paddingTop: "8rem", paddingBottom: "8rem" }}>
 					<div className="text-center">
-						<h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-							Data to enrich your online business
+						<h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">
+							Simplify Your Links, Amplify Your Reach
 						</h1>
-						<p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-							Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-							fugiat veniam occaecat.
+						<p className="mt-8 text-base font-medium text-pretty text-gray-500 sm:text-xl/8">
+							Transform long, complex URLs into short, shareable links. Perfect for businesses, social media, and personal use.
+						</p>
+						<p className="mt-4 text-md text-gray-600">
+    						Start shortening your URLs today and track their performance with detailed analytics.
 						</p>
 						<div className="mt-10 flex items-center justify-center gap-x-6">
 							<a
 								href="#"
-								className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+								className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
 							>
-								Get started
+								Shorten Your URL
 							</a>
-							<a href="#" className="text-sm/6 font-semibold text-gray-900">
+							<a href="https://github.com/Mfrozzz/Url-shortener" className="text-sm/6 font-semibold text-gray-900">
 								Learn more <span aria-hidden="true">→</span>
 							</a>
 						</div>
